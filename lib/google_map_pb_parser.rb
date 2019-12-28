12021203f561fbbbdf6a059c391570a0ffde62e4
fukata <tatsuya.fukata@gmail.com>
2019-12-29 02:45:44 +0700

    change data structure

--- lib/google_map_pb_parser.rb
+++ lib/google_map_pb_parser.rb
@@ -2,11 +2,88 @@ require "google_map_pb_parser/version"
 
 module GoogleMapPbParser
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.to_normal_url(embed_url)
+    embed_url = URI.unescape(embed_url)
+    if m = embed_url.match(%r|^https://(www\.google\.com)/maps/embed\?pb=(.+)|)
+      host = m[1]
+      pb = m[2]
+      result = parse(pb)
+
+      lat = result.dig(0, :value, 0, :value, 0, :value, 2, :value)
+      lng = result.dig(0, :value, 0, :value, 0, :value, 1, :value)
+      place_id = result.dig(0, :value, 1, :value, 0, :value, 0, :value)
+      data = decode_result(
+        [
+          {
+            id: '4',
+            type:'m',
+            value: [
+              {
+                id: '3',
+                type: 'm',
+                value: [
+                  {id: '1', type: 's', value: place_id},
+                  {
+                    id: '8',
+                    type: 'm',
+                    value: [
+                      {id: '3', type: 'd', value: lat},
+                      {id: '4', type: 'd', value: lng},
+                    ]
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      )
+
+      zoom = '17z'
+      url = "https://#{host}/maps/place/@#{lat},#{lng},#{zoom}/data=#{data}"
+      url
+    else
+      raise Error.new("Invalid url. url=#{embed_url}")
+    end
+  end
+
+  def self.decode_result(result)
+    data = ''
+    result.each do |v|
+      id = v[:id]
+      type = ''
+      value = ''
+      case v[:type]
+      when 'm'
+        type = 'm'
+        value = _map_size(v[:value]).to_s
+        data += "!#{id}#{type}#{value}"
+        data += decode_result(v[:value])
+      else
+        type = v[:type]
+        value = URI.escape(v[:value])
+        data += "!#{id}#{type}#{value}"
+      end
+    end
+    data
+  end
+
+  def self._map_size(result)
+    size = 0
+    result.each do |v|
+      case v[:type]
+      when 'm'
+        size += _map_size(v[:value]) + 1
+      else
+        size += 1
+      end
+    end
+    size
+  end
 
   def self.parse(pb)
     params = pb.split('!').reject{|s| s == nil || s == ''}
-    _parse({}, params)
+    _parse([], params)
   end
 
   def self._parse(result, params, from=0, to=0)
@@ -22,18 +99,22 @@ module GoogleMapPbParser
 
         case type
         when 'm'
-          result[id] = _parse({}, _params, i + 1, value.to_i)
+          result.push({
+            raw: param,
+            type: 'm',
+            value: _parse([], _params, i + 1, value.to_i)
+          })
           # Since it is added at the end of the loop, subtract 1 here.
           i += value.to_i
         else
-          result[id] = {
+          result.push({
             raw: param,
             type: type,
             value: value,
-          }
+          })
         end
       else
-        raise Error.new("parse error. param=#{param}")
+        raise Error.new("Parse error. param=#{param}")
       end
       i += 1
     end
--- spec/google_map_pb_parser_spec.rb
+++ spec/google_map_pb_parser_spec.rb
@@ -5,43 +5,85 @@ RSpec.describe GoogleMapPbParser do
 
   it "parse pb parameter" do
     pb = '!1m18!1m12!1m3!1d3717.560901381966!2d99.5956620927018!3d21.288842013619135!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30d4417bb9d2cb77:0x3aa5ca84cbdd9e7d!2sStanding%20Buddha!5e0!3m2!1sja!2sth!4v1576440979089!5m2!1sja!2sth'
-    expected = {
-      "1"=>{
-        "1"=>{
-          "1"=>{
-            "1"=>{:raw=>"1d3717.560901381966", :type=>"d", :value=>"3717.560901381966"},
-            "2"=>{:raw=>"2d99.5956620927018", :type=>"d", :value=>"99.5956620927018"},
-            "3"=>{:raw=>"3d21.288842013619135", :type=>"d", :value=>"21.288842013619135"}
+    expected = [
+      {
+        :raw=>"1m18",
+        :type=>"m",
+        :value=> [
+          {
+            :raw=>"1m12",
+            :type=>"m",
+            :value=> [
+              {
+                :raw=>"1m3",
+                :type=>"m",
+                :value=> [
+                  {:raw=>"1d3717.560901381966", :type=>"d", :value=>"3717.560901381966"},
+                  {:raw=>"2d99.5956620927018", :type=>"d", :value=>"99.5956620927018"},
+                  {:raw=>"3d21.288842013619135", :type=>"d", :value=>"21.288842013619135"}
+                ]
+              },
+              {
+                :raw=>"2m3",
+                :type=>"m",
+                :value=> [
+                  {:raw=>"1f0", :type=>"f", :value=>"0"},
+                  {:raw=>"2f0", :type=>"f", :value=>"0"},
+                  {:raw=>"3f0", :type=>"f", :value=>"0"}
+                ]
+              },
+              {
+                :raw=>"3m2",
+                :type=>"m",
+                :value=> [
+                  {:raw=>"1i1024", :type=>"i", :value=>"1024"},
+                  {:raw=>"2i768", :type=>"i", :value=>"768"}
+                ]
+              },
+              {:raw=>"4f13.1", :type=>"f", :value=>"13.1"}
+            ]
           },
-          "2"=>{
-            "1"=>{:raw=>"1f0", :type=>"f", :value=>"0"},
-            "2"=>{:raw=>"2f0", :type=>"f", :value=>"0"},
-            "3"=>{:raw=>"3f0", :type=>"f", :value=>"0"}
+          {
+            :raw=>"3m3",
+            :type=>"m",
+            :value=> [
+              {
+                :raw=>"1m2",
+                :type=>"m",
+                :value=> [
+                  {:raw=>"1s0x30d4417bb9d2cb77:0x3aa5ca84cbdd9e7d", :type=>"s", :value=>"0x30d4417bb9d2cb77:0x3aa5ca84cbdd9e7d"},
+                  {:raw=>"2sStanding%20Buddha", :type=>"s", :value=>"Standing%20Buddha"}
+                ]
+              }
+            ]
           },
-          "3"=>{
-            "1"=>{:raw=>"1i1024", :type=>"i", :value=>"1024"},
-            "2"=>{:raw=>"2i768", :type=>"i", :value=>"768"}
-          },
-          "4"=>{:raw=>"4f13.1", :type=>"f", :value=>"13.1"}
-        },
-        "3"=>{
-          "1"=>{
-            "1"=>{:raw=>"1s0x30d4417bb9d2cb77:0x3aa5ca84cbdd9e7d", :type=>"s", :value=>"0x30d4417bb9d2cb77:0x3aa5ca84cbdd9e7d"},
-            "2"=>{:raw=>"2sStanding%20Buddha", :type=>"s", :value=>"Standing%20Buddha"}
-          }
-        },
-        "5"=>{:raw=>"5e0", :type=>"e", :value=>"0"}
+          {:raw=>"5e0", :type=>"e", :value=>"0"}
+        ]
       },
-      "3"=>{
-        "1"=>{:raw=>"1sja", :type=>"s", :value=>"ja"},
-        "2"=>{:raw=>"2sth", :type=>"s", :value=>"th"}
+      {
+        :raw=>"3m2",
+        :type=>"m",
+        :value=> [
+          {:raw=>"1sja", :type=>"s", :value=>"ja"},
+          {:raw=>"2sth", :type=>"s", :value=>"th"}
+        ]
       },
-      "4"=>{:raw=>"4v1576440979089", :type=>"v", :value=>"1576440979089"},
-      "5"=>{
-        "1"=>{:raw=>"1sja", :type=>"s", :value=>"ja"},
-        "2"=>{:raw=>"2sth", :type=>"s", :value=>"th"}
+      {:raw=>"4v1576440979089", :type=>"v", :value=>"1576440979089"},
+      {
+        :raw=>"5m2",
+        :type=>"m",
+        :value=> [
+          {:raw=>"1sja", :type=>"s", :value=>"ja"},
+          {:raw=>"2sth", :type=>"s", :value=>"th"}
+        ]
       }
-    }
+    ]
     expect(GoogleMapPbParser.parse(pb)).to eq(expected)
+  end
+
+  it "embed url to normal url" do
+    embed_url = 'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3717.5945138538577!2d99.5945486!3d21.2875125!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30d4417bb9d2cb77%3A0x3aa5ca84cbdd9e7d!2sStanding%20Buddha!5e0!3m2!1sja!2sth!4v1577557617608!5m2!1sja!2sth'
+    expected = 'https://www.google.com/maps/place/@21.2875125,99.5945486,17z/data=!4m5!3m4!1s0x30d4417bb9d2cb77:0x3aa5ca84cbdd9e7d!8m2!3d21.2875125!4d99.5945486'
+    expect(GoogleMapPbParser.to_normal_url(embed_url)).to eq(expected)
   end
 end
